30ec9e7598861003422b0c2ab270f3a56119da48
unknown <lukus2013@yahoo.com>
2021-10-19 19:10:38 -0500

    nav completed

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,38 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase';
+import 'firebase/auth';
+import { Container } from 'reactstrap';
+import Navigation from '../components/Navigation';
+import SignIn from '../views/SignIn';
 
 function Initialize() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [user, setUser] = useState([]);
 
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
-  };
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfo = {
+          name: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0],
+        };
+        setUser(userInfo);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
 
   return (
-    <div className="App">
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          type="button"
-          id="this-button"
-          className="btn btn-info"
-          onClick={handleClick}
-        >
-          I am THIS button
-        </button>
-      </div>
-      <div>
-        <button
-          type="button"
-          id="that-button"
-          className="btn btn-primary mt-3"
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
-    </div>
+    <Container>
+      {user ? (
+        <>
+          <Navigation />
+          <h2>Create Your Fantasy Team</h2>
+          <h3>Think deeply and create the team of your dreams.</h3>
+          <h3>
+            Your roster could be real or fake, just make one that will win the
+            championship.
+          </h3>
+        </>
+      ) : (
+        <SignIn user={user} />
+      )}
+    </Container>
   );
 }
 
